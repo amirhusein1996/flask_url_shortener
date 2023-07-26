6088ba8e099f53f6503485ed1ccfa0e48e00b4e2
Amirhusein <amirhusein1996@gmail.com>
2023-07-27 00:05:23 +0330

    first shape of config

--- config.py
+++ config.py
@@ -1,1 +1,26 @@
-time_zone = 'UTC'
+import os
+
+
+class Config:
+    # Security
+    SECRET_KEY = os.getenv('SECRET_KEY', 'a-very-secret-key')
+
+    # Time Zone
+    TIME_ZONE = 'UTC'
+
+    # Base URL
+    BASE_URL = os.getenv('BASE_URL', 'http://localhost:5000')
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+}
