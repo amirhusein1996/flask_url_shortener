d1b5f4e2f0438040867b06b4879f9988b7e27b18
Amirhusein <amirhusein1996@gmail.com>
2023-07-27 00:10:24 +0330

    time zone got from Config class

--- app/models/short_urls.py
+++ app/models/short_urls.py
@@ -1,10 +1,10 @@
 from mongoengine.fields import Document, URLField, StringField, DateTimeField
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from config import time_zone
+from config import Config
 
 
 class URL(Document):
     origin_url = URLField(max_length=255)
     short_url = StringField(max_length=20)
-    created_at = DateTimeField(default=datetime.now(ZoneInfo(time_zone)))
+    created_at = DateTimeField(default=datetime.now(ZoneInfo(Config.TIME_ZONE)))
