3db1497c659dd9ed45e045189bdc7fbba7b72b99
Amirhusein <amirhusein1996@gmail.com>
2023-07-27 15:19:37 +0330

    process_origin_url is done

--- app/routers/index.py
+++ app/routers/index.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, render_template_string, jsonify
+from mongoengine.errors import ValidationError
 from app.forms.url_form import URLForm
+from app.models.short_urls import URL
+from app.utils.random import generate_random_string
 
 
 blue_print = Blueprint(
@@ -20,5 +23,51 @@ def index_page():
 
 
 @blue_print.post('/')
-def process_origin_url(origin_url):
-    pass
+def process_origin_url():
+    form = URLForm()
+
+    if form.validate_on_submit():
+        origin_url = form.origin_url
+        url = URL.objects(origin_url=origin_url).first()
+
+        if url:
+            temp = render_template_string(
+                '_url_table.html',
+                url=url
+            )
+            return jsonify(
+                {
+                    'message': temp
+                }
+            )
+
+        try:
+            url = URL(
+                origin_url=origin_url,
+                short_url = generate_random_string()
+                )
+            url.save()
+            temp = render_template_string(
+                '_url_table.html',
+                url=url
+            )
+
+            return jsonify(
+                {
+                    'message': temp
+                }
+            )
+
+        except ValidationError as e:
+
+            return jsonify(
+                {
+                    'message': e.message
+                }
+            )
+
+    return jsonify(
+        {
+            'message': 'Invalid inputs'
+        }
+    ), 400
