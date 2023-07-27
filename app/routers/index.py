59c0c0c111faa539272725abdc282b72827c19c6
Amirhusein <amirhusein1996@gmail.com>
2023-07-27 03:56:06 +0330

    index page is done

--- app/routers/index.py
+++ app/routers/index.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, render_template_string, jsonify
+from app.forms.url_form import URLForm
+
 
 blue_print = Blueprint(
     name='index',
@@ -7,10 +9,16 @@ blue_print = Blueprint(
     template_folder='../templates'
 )
 
+
 @blue_print.get('/')
 def index_page():
-    pass
+    form = URLForm()
+    return render_template(
+        template_name_or_list='index.html',
+        form=form
+    )
 
-@blue_print.post('/<path:origin_url')
+
+@blue_print.post('/')
 def process_origin_url(origin_url):
     pass
