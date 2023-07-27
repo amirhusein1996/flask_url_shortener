7f5f196d60826d802f2b372147d7fe7a150a8322
Amirhusein <amirhusein1996@gmail.com>
2023-07-27 04:34:40 +0330

    total_visit field added

--- app/models/short_urls.py
+++ app/models/short_urls.py
@@ -1,4 +1,4 @@
-from mongoengine.fields import Document, URLField, StringField, DateTimeField
+from mongoengine.fields import Document, URLField, StringField, DateTimeField ,IntField
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from config import Config
@@ -7,4 +7,5 @@ from config import Config
 class URL(Document):
     origin_url = URLField(max_length=255)
     short_url = StringField(max_length=20)
+    total_visit = IntField(default=0 , min_value=0)
     created_at = DateTimeField(default=datetime.now(ZoneInfo(Config.TIME_ZONE)))
